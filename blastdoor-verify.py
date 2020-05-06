1158b94c1bf5be41080fd94b54ce783b81f0cd34
redlolz <robinlego@hotmail.nl>
2020-05-07 01:59:57 +0200

    Initial commit

--- blastdoor-verify.py
+++ blastdoor-verify.py
@@ -7,52 +7,52 @@ from riposte.printer import Palette
 import os.path
 import sys
 
-peek = Riposte('[blastdoor]$ ')
+blast = Riposte('[blastdoor]$ ')
 
-@peek.command('help')
+@blast.command('help')
 def help():
-    peek.status("List of commands")
-    peek.status("generate <filename>       - Generate a private RSA key")
-    peek.status("publickey <filename>      - Generate public key from private key")
-    peek.status("sign <filename> <message> - Sign message with private key")
-    peek.status("exit                      - Exit the program")
+    blast.status("List of commands")
+    blast.status("generate <filename>       - Generate a private RSA key")
+    blast.status("publickey <filename>      - Generate public key from private key")
+    blast.status("sign <filename> <message> - Sign message with private key")
+    blast.status("exit                      - Exit the program")
 
-@peek.command('generate')
+@blast.command('generate')
 def generate(filename: str):
     if not os.path.isfile(filename):
         # Generate RSA keypair
-        peek.status("Generating RSA key...")
+        blast.status("Generating RSA key...")
         key = RSA.generate(4096)
         enc_key = key.export_key()
 
-        peek.status("Writing private key file...")
+        blast.status("Writing private key file...")
         public_key_file = open(filename, 'wb')
         public_key_file.write(enc_key)
-        peek.success("Done")
+        blast.success("Done")
     else:
-        peek.error("File already exists!")
+        blast.error("File already exists!")
 
-@peek.command('publickey')
-def generate(filename: str):
+@blast.command('publickey')
+def publickey(filename: str):
     if os.path.isfile(filename):
         key = RSA.import_key(open(filename).read())
-        peek.print(Palette.CYAN.format(key.publickey().export_key().decode('utf-8')))
+        blast.print(Palette.CYAN.format(key.publickey().export_key().decode('utf-8')))
     else:
-        peek.error("File doesn't exist!")
+        blast.error("File doesn't exist!")
 
-@peek.command('sign')
+@blast.command('sign')
 def sign(filename: str, message: str):
     if os.path.isfile(filename):
         key = RSA.import_key(open(filename).read())
         h = SHA256.new(message.encode('utf-8'))
         signature = pkcs1_15.new(key).sign(h)
-        peek.print(Palette.CYAN.format(b64encode(signature).decode('utf-8')))
+        blast.print(Palette.CYAN.format(b64encode(signature).decode('utf-8')))
     else:
-        peek.error("File doesn't exists!")
+        blast.error("File doesn't exists!")
 
-@peek.command('exit')
+@blast.command('exit')
 def exit():
-    peek.status("Goobye!")
+    blast.status("Goobye!")
     sys.exit()
 
-peek.run()
+blast.run()
